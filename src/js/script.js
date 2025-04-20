b5313a9a1e2c3244b7973c2764540cda2777cce5
TORU TAKASE <toru1takase1@gmail.com>
2025-04-20 13:37:22 +0900

    0420

--- src/js/script.js
+++ src/js/script.js
@@ -187,9 +187,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //アコーディオン(faq)
 jQuery(function ($) {
-  $(".faq-list:first-of-type .faq-list__answer").css("display", "block");
-  // はじめのアコーディオンを開いておく
-  $(".faq-list:first-of-type .faq-list__question").addClass("is-open");  
+  $(".faq-list .faq-list__answer").css("display", "block");
+  // アコーディオンを開いておく
+  $(".faq-list .faq-list__question").addClass("is-open");  
   $(".js-faq-list").on("click", function () {
     $(this).next().slideToggle(200);
     $(this).toggleClass("is-open", 200);
